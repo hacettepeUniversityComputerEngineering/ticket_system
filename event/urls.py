135f1dac4e4d531aa852ad0a1ca2c2decf795bb8
sdbyrk <sd.byrk@hotmail.com>
2019-01-11 13:00:25 +0300

    Login, logout, signup added.

--- event/urls.py
+++ event/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
     path('event_details', views.deneme_event, name='event_page'),
     path('', views.deneme_home, name='home_page'),
-
+    url(r'^signup/$', views.signup, name='signup'),
 ]
--- event/views.py
+++ event/views.py
@@ -1,9 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+
+
 def deneme_event(request):
     return render(request, 'event_details.html', {})
+
 
 def deneme_home(request):
     return render(request, 'home.html', {})
 
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            raw_password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home_page')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
